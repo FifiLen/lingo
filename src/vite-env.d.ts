b0ad59e474986533fa314e50ca49ff75fa8c003a
FifiLen <filippraca02@gmail.com>
2023-12-15 13:47:58 +0100

    will it work?

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+
+declare module 'react-dom' {
+    interface ReactDOM {
+      createRoot(container: Element | Document | DocumentFragment | null, options?: RootOptions): ReactDOMRoot;
+    }
+  
+    const createRoot: ReactDOM['createRoot'];
+  }
